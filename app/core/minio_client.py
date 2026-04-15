c32ba6ae23a2c6fcf2807ed58fb1851021b1748b
Pascal <pas.grimm.23@heilbronn.dhbw.de>
2026-04-15 13:29:47 +0200

    next try to fix minio

--- app/core/minio_client.py
+++ app/core/minio_client.py
@@ -9,7 +9,7 @@ def get_minio_client() -> Minio:
         settings.MINIO_ENDPOINT,
         access_key=settings.MINIO_ACCESS_KEY,
         secret_key=settings.MINIO_SECRET_KEY,
-        secure=settings.MINIO_SECURE,
+        secure=False,
     )
 
 
@@ -19,7 +19,7 @@ def get_minio_public_client() -> Minio:
         settings.MINIO_PUBLIC_ENDPOINT,
         access_key=settings.MINIO_ACCESS_KEY,
         secret_key=settings.MINIO_SECRET_KEY,
-        secure=settings.MINIO_SECURE,
+        secure=True,
     )
 
 
